344337a1af4471f509c6937efa1c7741f8ee3b6d
Jaime Lazo <lazoferman.jaime@gmail.com>
2025-05-29 23:38:36 -0600

    Update 10

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,28 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService,
+    ) { }
 
-  async validateUser(email: string, password: string): Promise<any> {
-    const user = await this.usersService.findOneByEmail(email);
-    console.log('Datos recibidos:', { email, password });
-    console.log('Usuario encontrado:', user);
 
-    // ⚠️ Comparación sin bcrypt (solo para pruebas locales)
-    if (user && user.password === password) {
-      return user;
+    async validateUser(email: string, password: string): Promise<any> {
+        const user = await this.usersService.findOneByEmail(email);
+        console.log('Datos recibidos:', { email, password });
+        console.log('Usuario encontrado:', user);
+
+        // ⚠️ Comparación sin bcrypt (solo para pruebas locales)
+        if (user && user.password === password) {
+            return user;
+        }
+
+        return null;
     }
 
-    return null;
-  }
+    async login(user: any) {
+        const payload = { email: user.email, sub: user.id };
 
-  async login(user: any) {
-    // tu lógica de generación de token aquí
-    return {
-      access_token: 'aqui_va_el_token', // reemplaza con tu lógica real
-      user,
-    };
-  }
+        const { password, ...userWithoutPassword } = user;
+
+        return {
+            access_token: this.jwtService.sign(payload),
+            user: userWithoutPassword,
+        };
+    }
+
 }
